1188aaecbc351988d37404283279ceb030c9ff52
PhantomCodeGhost <155839780+PhantomCodeGhost@users.noreply.github.com>
2025-03-10 18:33:32 +0530

    Add files via upload

---
+++ C++ Files/Slips/Slip_5.cpp
@@ -0,0 +1,26 @@
+/* Q1.Write a C++ program to create a class Mobile which contains data members as Mobile_Id,
+Mobile_Name, Mobile_Price. Create and Initialize all values of Mobile object by using
+parameterized constructor. Display the values of Mobile object. 
+*/
+
+
+
+/* Q2.Create a base class Student (Roll_No, Name) which derives two classes Theory and Practical.
+Theory class contains marks of five Subjects and Practical class contains marks of two practical
+subjects. Class Result (Total_Marks, Class) inherits both Theory and Practical classes. (Use
+concept of Virtual Base Class) Write a menu driven program to perform the following functions:
+a. Build a master table.
+b. Display master table. */
+
+
+// ------------------- OR ---------------------------------
+
+
+
+
+
+/* Q2.Create a class Book containing Book_name, author and Price as a data member and write
+necessary member functions for the following (use function overloading).
+a. To Accept and display the Book Information.
+b. Display book details of a given author
+c. Display book details of specific price */
