357cbfcf764ed1af79416c95bf7fe142c7df3191
PhantomCodeGhost <155839780+PhantomCodeGhost@users.noreply.github.com>
2025-10-17 00:32:35 +0530

    Update Slip_01.c

--- Os Files/Slip_01.c
+++ Os Files/Slip_01.c
@@ -1,94 +1,4 @@
-// Q1 Write a program that demonstrates the use of nice() system call. After a child process is started using fork(), assign higher priority to the child using nice() system call.   
-
-#include <stdio.h>      // For input/output functions: printf()
-#include <stdlib.h>     // For general utilities: nice() function
-#include <unistd.h>     // For POSIX OS API: fork(), getpid(), sleep(), nice()
-#include <sys/types.h>  // For system data types: pid_t (used by fork() and getpid())
-#include <sys/wait.h>   // For process control: wait() function
-
-int main(){
-    int pid, retnice;
-    pid = fork();
-    
-    if(pid == 0){
-        retnice = nice(-2);
-        printf("Child Process (PID : %d ), nice value %d\n", getpid(), retnice);
-        sleep(1);
-    } else {
-        retnice = nice(5);
-        printf("Parent process (
-            PID: %d), nice value %d\n", getpid(), retnice);
-        wait(NULL);
-    }
-    
-    return 0;
-}
-
-NOTE : if Pid is 0 : Child creatd, 1 = Parent created, <0 = fork failed.
-
-
-
-x-----x
-
-// Q2 FIFO
-
-#include <stdio.h>
-int main(){
-    // incoming Stream : Sequence of Page requests
-    int incomingStream[] = {3, 4, 5, 6, 3, 4, 7, 3, 4, 5, 6,7, 2, 4, 6};
-    int pageFaults = 0;
-    int frames = 3;
-    
-    int m, n, s, pages;
-    pages = sizeof(incomingStream)/sizeof(incomingStream[0]);
-    printf("Incoming \t Frame 1 \t Frame 2 \t Frame 3\n");
-    
-    int temp[frames];
-    int pointer = 0; // FIFO pointer to track which frame to replace next
-    
-    //Initialize all frames as empty(-1)
-    for(m = 0; m < frames; m++){
-        temp[m] = -1;
-    }    
-    
-    for(m = 0; m < pages; m++){
-        s = 0;  // Reset the Page Found flag
-        
-        // Check if pages already in frames        
-        for(n = 0; n < frames; n++){
-            if(incomingStream[m] == temp[n]){
-                s = 1; // Page Found
-                break;
-            }
-        }
-        //Page not found (Page Fault s = 0)
-        if(s == 0){
-            temp[pointer] = incomingStream[m];
-            pointer = (pointer + 1) % frames; //Move pointer circularly
-            pageFaults++;
-        }
-        
-        printf("\t%d\t\t",incomingStream[m]);
-        for(n = 0; n < frames; n++){
-            if(temp[n] != -1)
-                printf("\t%d\t\t", temp[n]);
-            else
-                printf("\t-\t\t");
-        }
-        
-        printf("\n");
-    }
-    printf("\nTotal Page Faults: %d\n", pageFaults);
-    return 0;       
-}
-
-
-x-----x
-
-
-
-/* Q3
-*/
+//Banker
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -171,6 +81,7 @@ int main() {
     
     return 0;
 }
+
 
 
 
